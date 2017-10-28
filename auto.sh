5ac20a041848d52814cdc42774b6f6d0c25e21f8
Cathy <icat.kw@gmail.com>
2017-10-28 22:04:18 +0800

    This is the third time trying to push the file

--- auto.sh
+++ auto.sh
@@ -10,7 +10,7 @@ for f in $FILES
 do
 /mingw64/bin/git add $f
 /mingw64/bin/git commit -m "$(date +%H-%M-%S)"
-/mingw64/bin/git remote add origin git@github.com:bsslc2/iii.git
+/mingw64/bin/git remote add origin git@github.com:bsslc2/thirdTry.git
 /mingw64/bin/git push -u origin master
 /mingw64/bin/git remote remove origin
 done
