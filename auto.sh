8b79b8168999fa36da6285e2e0249b3faac0ba04
Cathy <icat.kw@gmail.com>
2017-10-28 21:32:54 +0800

    try github upload, good try

---
+++ auto.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+cd /c/Users/Java/git/repo1
+while true
+do
+fc="$(ls | wc -l)"
+if [ $fc -gt 0 ];
+then
+FILES=/c/Users/Java/git/repo1/*
+for f in $FILES
+do
+/mingw64/bin/git add $f
+/mingw64/bin/git commit -m "$(date +%H-%M-%S)"
+/mingw64/bin/git remote add origin git@github.com:bsslc2/iii.git
+/mingw64/bin/git push -u origin master
+/mingw64/bin/git remote remove origin
+done
+fi;
+rm -rf /c/Users/Java/git/repo1/*
+sleep 3
+done
